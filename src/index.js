5420043f89f7935734c561ff6ee9c0f41e4458c2
Carylen <irham.atmoko@ids.id>
2024-06-09 17:24:45 +0700

    commit, Supabase and Vercel Config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { PrismaClient } = require("@prisma/client");
+// const bodyParser = require('body-parser')
 
-dotenv.config();
+
 
 const app = express();
 app.use(express.json());
-const prisma = new PrismaClient()
 
+const prisma = new PrismaClient();
+
+dotenv.config();
 PORT = process.env.PORT;
 
-app.get('/users', async(req, res) => {
-    const allUser = await prisma.user.findMany()
+app.get("/users", async (req, res) => {
+  const allUser = await prisma.user.findMany();
 
-    res.json(allUser)
-})
+  res.send(allUser);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  const userDetail = await prisma.user.findUnique({
+    where: {
+      id: parseInt(userId)
+    },
+  });
+
+  res.send(userDetail);
+});
 
 app.listen(PORT, () => {
   console.log(`Server are running on PORT : ${PORT}`);
