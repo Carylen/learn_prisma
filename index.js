2b03af132eb5f72f0abaf77b0a61095b639a7d50
Carylen <irham.atmoko@ids.id>
2024-06-09 18:47:23 +0700

    commit, Supabase and Vercel Config

--- index.js
+++ index.js
@@ -16,19 +16,32 @@ PORT = process.env.PORT;
 app.get("/users", async (req, res) => {
   const allUser = await prisma.user.findMany();
 
+  console.log(`Fetching All User : ${JSON.stringify(allUser)}`)
   res.send(allUser);
 });
 
 app.get("/users/:id", async (req, res) => {
   const userId = req.params.id;
 
-  const userDetail = await prisma.user.findUnique({
-    where: {
-      id: parseInt(userId)
-    },
-  });
+  try {
+    const userDetail = await prisma.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+    });
 
-  res.send(userDetail);
+    if (userDetail) {
+      console.log(`user found: ${JSON.stringify(userDetail)}`);
+      res.json(userDetail);
+    } else {
+      console.log("User not found");
+      res.status(404).json({ error: "User not found" });
+    }
+  } catch (error) {
+    console.error(`Error : ${error.message}`)
+    res.status(500).send('Internal Server Error')
+  }
+  
 });
 
 app.listen(PORT, () => {
